48583b1f86e8b70c5032ec8d120559dad8ab1720
Robertosnap <robertosnap@pm.me>
2024-06-18 22:18:19 +0200

    Trying with sqc

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [react({
+		tsDecorators: true
+	}), sveltekit()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
-	}
+	},
+	// optimizeDeps: {
+	// 	entries: ['class-validator', 'class-transformer', '@credo-ts/askar', '@credo-ts/core', '@credo-ts/node', '@credo-ts/openid4vc', '@credo-ts/tenants']
+	// }
 });
