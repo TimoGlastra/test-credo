d68f7282bd4d7ad5430ee3f583bc9225d8ec8f5f
Robertosnap <robertosnap@pm.me>
2024-06-18 13:43:47 +0200

    Add plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,26 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import ts from 'typescript';
+import { vitePluginTypescriptTransform } from 'vite-plugin-typescript-transform';
 import { defineConfig } from 'vitest/config';
 
+
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [sveltekit(), vitePluginTypescriptTransform({
+		enforce: 'pre',
+		filter: {
+			files: {
+				include: /\.ts$/,
+			},
+		},
+		tsconfig: {
+			override: {
+				target: ts.ScriptTarget.ES2021,
+			},
+		},
+	})],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
 });
+
+
