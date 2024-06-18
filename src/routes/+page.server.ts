b908048011b5331d203635377f1629aeabd59b9c
Robertosnap <robertosnap@pm.me>
2024-06-18 13:46:02 +0200

    Without credo deps

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,10 +1,10 @@
-import { getRootAgent } from "$lib/agent/agent";
-import { ariesAskar } from "@hyperledger/aries-askar-nodejs";
+// import { getRootAgent } from "$lib/agent/agent";
+// import { ariesAskar } from "@hyperledger/aries-askar-nodejs";
 
 
 export const load = async ({ url }) => {
 	console.log("Server running", url.toString())
-	const rootAgent = await getRootAgent(ariesAskar.storeGenerateRawKey({}));
-	await rootAgent.shutdown()
+	// const rootAgent = await getRootAgent(ariesAskar.storeGenerateRawKey({}));
+	// await rootAgent.shutdown()
 	console.log("Server shutdown")
 };
